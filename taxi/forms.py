f6b986f81c67b09c710ce1da59c1ddadd169082b
Alex <cornijoleksandr414@gmail.com>
2026-04-17 09:31:45 +0200

    Fix validation, crispy forms and cancel buttons

--- taxi/forms.py
+++ taxi/forms.py
@@ -13,16 +13,54 @@ class DriverCreateForm(UserCreationForm):
         model = User
         fields = UserCreationForm.Meta.fields + ("license_number",)
 
+    def clean_license_number(self):
+        license_number = self.cleaned_data["license_number"]
+
+        if len(license_number) != 8:
+            raise forms.ValidationError(
+                "License must consist of 8 characters"
+            )
+
+        if not (
+            license_number[:3].isalpha()
+            and license_number[:3].isupper()
+            and license_number[3:].isdigit()
+        ):
+            raise forms.ValidationError(
+                "License must be 3 uppercase letters followed by 5 digits"
+            )
+
+        return license_number
+
 
 class DriverLicenseUpdateForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ("license_number",)
 
+    def clean_license_number(self):
+        license_number = self.cleaned_data["license_number"]
+
+        if len(license_number) != 8:
+            raise forms.ValidationError(
+                "License must consist of 8 characters"
+            )
+
+        if not (
+            license_number[:3].isalpha()
+            and license_number[:3].isupper()
+            and license_number[3:].isdigit()
+        ):
+            raise forms.ValidationError(
+                "License must be 3 uppercase letters followed by 5 digits"
+            )
+
+        return license_number
+
 
 class CarForm(forms.ModelForm):
     drivers = forms.ModelMultipleChoiceField(
-        queryset=get_user_model().objects.all(),
+        queryset=User.objects.all(),
         widget=forms.CheckboxSelectMultiple,
         required=False
     )
